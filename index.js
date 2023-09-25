44f7a8bafa5b5b89ee8256ccd6f32c52a63c706f
Pranav Waykar <90698028+pranavwaykar@users.noreply.github.com>
2023-09-25 17:47:18 +0530

    Add files via upload
    Adding Codebase

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+const axios = require("axios");
+const twilio = require("twilio");
+
+module.exports.handler = async (event) => {
+  const {
+    NASA_API_KEY,
+    TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN,
+    TWILIO_PHONE_NUMBER,
+    MY_PHONE_NUMBER,
+  } = process.env;
+
+  const { data: imageData } = await axios.get(
+    `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
+  );
+
+  const body = `
+  --${imageData.data}--
+  Good Morning!
+  Today's NASA Image From Space Is
+  -----
+  "${imageData.title}"
+  -----
+  ${imageData.explanation}
+  `;
+
+  const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+  await client,
+    messages.create({
+      body,
+      mediaUrl: [imageData.url],
+      from: TWILIO_PHONE_NUMBER,
+      to: MY_PHONE_NUMBER,
+    });
+};
